99629879450752848395f6ba75bac48df0c7ba94
Alexei Kornienko <alexei.kornienko@gmail.com>
2020-08-25 00:00:25 +0300

    Fixed test for PUB/SUB sockets

--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,15 @@
 use crate::{BlockingRecv, BlockingSend, NonBlockingSend, Socket, SocketFrontend};
+use futures::channel::{mpsc, oneshot};
 use std::convert::TryInto;
 use std::error::Error;
 use std::time::Duration;
+use futures::{StreamExt, SinkExt};
+use chrono::Utc;
 
 #[tokio::test]
 async fn test_pub_sub_sockets() {
+    let (server_stop_sender, mut server_stop) = oneshot::channel::<()>();
+    let (results_sender, results) = mpsc::channel(100);
     tokio::spawn(async move {
         let mut pub_socket = crate::PubSocket::new();
         pub_socket
@@ -12,31 +17,41 @@ async fn test_pub_sub_sockets() {
             .await
             .expect("Failed to bind socket");
 
-        for i in 0..10i32 {
-            let message = format!("Message - {}", i);
-            pub_socket
-                .send(message.into())
-                .expect("Failed to send");
+        loop {
+            if let Ok(Some(_)) = server_stop.try_recv() {
+                break;
+            }
+            pub_socket.send(Utc::now().to_rfc2822().into()).expect("Failed to send");
+            tokio::time::delay_for(Duration::from_millis(100)).await;
         }
     });
 
-    tokio::spawn(async move {
-        let mut sub_socket = crate::SubSocket::connect("127.0.0.1:5556")
-            .await
-            .expect("Failed to connect");
-
-        sub_socket.subscribe("").await.expect("Failed to subscribe");
-
-        for i in 0..10i32 {
-            let repl: String = sub_socket
-                .recv()
+    for _ in 0..10 {
+        let mut client_sender = results_sender.clone();
+        tokio::spawn(async move {
+            let mut sub_socket = crate::SubSocket::connect("127.0.0.1:5556")
                 .await
-                .expect("Failed to recv")
-                .try_into()
-                .expect("Malformed string");
-            assert_eq!(format!("Message - {}", i), repl)
-        }
-    });
+                .expect("Failed to connect");
+
+            sub_socket.subscribe("").await.expect("Failed to subscribe");
+
+            for _ in 0..10i32 {
+                let repl: String = sub_socket
+                    .recv()
+                    .await
+                    .expect("Failed to recv")
+                    .try_into()
+                    .expect("Malformed string");
+                assert_eq!(Utc::now().to_rfc2822(), repl);
+                client_sender.send(true).await.unwrap();
+            }
+        });
+    }
+    drop(results_sender);
+
+    let res_vec: Vec<bool> = results.collect().await;
+    server_stop_sender.send(()).unwrap();
+    assert_eq!(100, res_vec.len());
 }
 
 async fn run_rep_server() -> Result<(), Box<dyn Error>> {
