28af2241d4e63385619ebf7be39a8e83a68b470c
Alexei Kornienko <alexei.kornienko@gmail.com>
2020-08-24 13:32:49 +0300

    Added thiserror crate and refactor errors to use it

--- src/error.rs
+++ src/error.rs
@@ -1,49 +1,32 @@
 use crate::codec::Message;
-use std::error::Error;
-use std::fmt::Display;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum ZmqError {
-    Network(String),
+    #[error("Malformed socket address")]
+    Address(#[from] std::net::AddrParseError),
+    #[error("Network error")]
+    Network(#[from] std::io::Error),
+    #[error("{0}")]
     Codec(&'static str),
+    #[error("{0}")]
     Socket(&'static str),
+    #[error("{0}")]
+    BufferFull(&'static str),
+    #[error("{0}")]
     Other(&'static str),
+    #[error("No message received")]
     NoMessage,
-}
-
-impl Display for ZmqError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        return match self {
-            ZmqError::Network(message) => write!(f, "{}", message),
-            ZmqError::Other(message) => write!(f, "{}", message),
-            ZmqError::Codec(message) => write!(f, "{}", message),
-            ZmqError::Socket(message) => write!(f, "{}", message),
-            ZmqError::NoMessage => write!(f, "No data received"),
-        };
-    }
-}
-
-impl Error for ZmqError {}
-
-impl From<std::net::AddrParseError> for ZmqError {
-    fn from(reason: std::net::AddrParseError) -> Self {
-        ZmqError::Network(reason.to_string())
-    }
-}
-impl From<std::io::Error> for ZmqError {
-    fn from(reason: std::io::Error) -> Self {
-        ZmqError::Network(reason.to_string())
-    }
 }
 
 impl From<futures::channel::mpsc::TrySendError<Message>> for ZmqError {
     fn from(_: futures::channel::mpsc::TrySendError<Message>) -> Self {
-        ZmqError::Other("Failed to send message. Send queue full")
+        ZmqError::BufferFull("Failed to send message. Send queue full/broken")
     }
 }
 
 impl From<futures::channel::mpsc::SendError> for ZmqError {
     fn from(_: futures::channel::mpsc::SendError) -> Self {
-        ZmqError::Other("Failed to send message. Send queue full/broken")
+        ZmqError::BufferFull("Failed to send message. Send queue full/broken")
     }
 }
