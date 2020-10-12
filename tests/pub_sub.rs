0efa9c3c9cfafc72c266b07ff23afbe655685652
Ryan Butler <thebutlah@gmail.com>
2020-10-12 08:03:27 -0400

    Made pub_sub test also test port 0

--- tests/pub_sub.rs
+++ tests/pub_sub.rs
@@ -2,7 +2,9 @@ use futures::channel::{mpsc, oneshot};
 use futures::{SinkExt, StreamExt};
 use std::convert::TryInto;
 use std::time::Duration;
+
 use zeromq::prelude::*;
+use zeromq::Endpoint;
 
 #[tokio::test]
 async fn test_pub_sub_sockets() {
@@ -11,14 +13,16 @@ async fn test_pub_sub_sockets() {
 
         let cloned_payload = payload.clone();
         let (server_stop_sender, mut server_stop) = oneshot::channel::<()>();
-        let (has_bound_sender, has_bound) = oneshot::channel::<()>();
+        let (has_bound_sender, has_bound) = oneshot::channel::<Endpoint>();
         tokio::spawn(async move {
             let mut pub_socket = zeromq::PubSocket::new();
-            pub_socket
+            let bound_to = pub_socket
                 .bind(bind_addr)
                 .await
                 .unwrap_or_else(|_| panic!("Failed to bind to {}", bind_addr));
-            has_bound_sender.send(()).expect("channel was dropped");
+            has_bound_sender
+                .send(bound_to)
+                .expect("channel was dropped");
 
             loop {
                 if let Ok(Some(_)) = server_stop.try_recv() {
@@ -34,16 +38,17 @@ async fn test_pub_sub_sockets() {
         // Block until the pub has finished binding
         // TODO: ZMQ sockets should not care about this sort of ordering.
         // See https://github.com/zeromq/zmq.rs/issues/73
-        has_bound.await.expect("channel was cancelled");
+        let bound_addr = has_bound.await.expect("channel was cancelled");
 
         let (sub_results_sender, sub_results) = mpsc::channel(100);
         for _ in 0..10 {
             let mut cloned_sub_sender = sub_results_sender.clone();
             let cloned_payload = payload.clone();
+            let cloned_bound_addr = bound_addr.clone();
             tokio::spawn(async move {
                 let mut sub_socket = zeromq::SubSocket::new();
                 sub_socket
-                    .connect(bind_addr)
+                    .connect(cloned_bound_addr)
                     .await
                     .unwrap_or_else(|_| panic!("Failed to connect to {}", bind_addr));
 
@@ -73,6 +78,9 @@ async fn test_pub_sub_sockets() {
         "tcp://127.0.0.1:5554",
         "tcp://[::1]:5555",
         "tcp://127.0.0.1:5556",
+        "tcp://localhost:0",
+        "tcp://127.0.0.1:0",
+        "tcp://[::1]:0",
     ];
     futures::future::join_all(addrs.into_iter().map(helper)).await;
 }
