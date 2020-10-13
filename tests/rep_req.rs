cf5f6bc8c40ec038c4b7f99706085de97cbe525f
Ryan Butler <thebutlah@gmail.com>
2020-10-12 22:21:45 -0400

    Tests now use 0 port where possible to avoid collisions

--- tests/rep_req.rs
+++ tests/rep_req.rs
@@ -1,11 +1,11 @@
+use zeromq::prelude::*;
+use zeromq::RepSocket;
+
 use std::convert::TryInto;
 use std::error::Error;
 use std::time::Duration;
-use zeromq::prelude::*;
 
-async fn run_rep_server() -> Result<(), Box<dyn Error>> {
-    let mut rep_socket = zeromq::RepSocket::new();
-    rep_socket.bind("tcp://127.0.0.1:5557").await?;
+async fn run_rep_server(mut rep_socket: RepSocket) -> Result<(), Box<dyn Error>> {
     println!("Started rep server on tcp://127.0.0.1:5557");
 
     for i in 0..10i32 {
@@ -19,14 +19,16 @@ async fn run_rep_server() -> Result<(), Box<dyn Error>> {
 
 #[tokio::test]
 async fn test_req_rep_sockets() -> Result<(), Box<dyn Error>> {
-    tokio::spawn(async move {
-        run_rep_server().await.unwrap();
+    let mut rep_socket = zeromq::RepSocket::new();
+    let endpoint = rep_socket.bind("tcp://localhost:0").await?;
+    println!("Started rep server on {}", endpoint);
+
+    tokio::spawn(async {
+        run_rep_server(rep_socket).await.unwrap();
     });
 
-    // yield for a moment to ensure that server has some time to open socket
-    tokio::time::delay_for(Duration::from_millis(10)).await;
     let mut req_socket = zeromq::ReqSocket::new();
-    req_socket.connect("tcp://127.0.0.1:5557").await?;
+    req_socket.connect(endpoint).await?;
 
     for i in 0..10i32 {
         req_socket.send(format!("Req - {}", i).into()).await?;
@@ -38,12 +40,17 @@ async fn test_req_rep_sockets() -> Result<(), Box<dyn Error>> {
 
 #[tokio::test]
 async fn test_many_req_rep_sockets() -> Result<(), Box<dyn Error>> {
+    let mut rep_socket = zeromq::RepSocket::new();
+    let endpoint = rep_socket.bind("tcp://localhost:0").await?;
+    println!("Started rep server on {}", endpoint);
+
     for i in 0..100i32 {
+        let cloned_endpoint = endpoint.clone();
         tokio::spawn(async move {
             // yield for a moment to ensure that server has some time to open socket
             tokio::time::delay_for(Duration::from_millis(100)).await;
             let mut req_socket = zeromq::ReqSocket::new();
-            req_socket.connect("tcp://127.0.0.1:5558").await.unwrap();
+            req_socket.connect(cloned_endpoint).await.unwrap();
 
             for j in 0..100i32 {
                 req_socket
@@ -56,10 +63,6 @@ async fn test_many_req_rep_sockets() -> Result<(), Box<dyn Error>> {
             drop(req_socket);
         });
     }
-
-    let mut rep_socket = zeromq::RepSocket::new();
-    rep_socket.bind("tcp://127.0.0.1:5558").await?;
-    println!("Started rep server on tcp://127.0.0.1:5558");
 
     for _ in 0..10000i32 {
         let mess: String = rep_socket.recv().await?.try_into()?;
