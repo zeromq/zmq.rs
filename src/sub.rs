9d5ea28e064eb19337ca854405e4e0f4be98a489
Dimitar Petrov <petrov.dimp@gmail.com>
2021-12-04 15:47:54 +0200

    refactor: move subscribe/unsubscribe to the backend
    - add create_subs_message associated function no `SubSocketBackend`
    - refactor `peer_connected` to avoid extra copy on iter of `Mutex<HashSet>`
    - use send_queue from `FramedIo` instead of getting a peer object
    - run `cargo clippy --fix`

--- src/sub.rs
+++ src/sub.rs
@@ -9,17 +9,22 @@ use crate::{
 };
 
 use crate::backend::Peer;
-use dashmap::DashMap;
-use parking_lot::Mutex;
-use crate::fair_queue::QueueInner;
 use crate::fair_queue::FairQueue;
-use crossbeam::queue::SegQueue;
+use crate::fair_queue::QueueInner;
 use async_trait::async_trait;
 use bytes::{BufMut, BytesMut};
+use crossbeam::queue::SegQueue;
+use dashmap::DashMap;
 use futures::channel::mpsc;
 use futures::{SinkExt, StreamExt};
+use parking_lot::Mutex;
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
+
+pub enum SubBackendMsgType {
+    UNSUBSCRIBE = 0,
+    SUBSCRIBE = 1
+}
 
 pub(crate) struct SubSocketBackend {
     pub(crate) peers: DashMap<PeerIdentity, Peer>,
@@ -44,8 +49,16 @@ impl SubSocketBackend {
             socket_type,
             socket_options: options,
             socket_monitor: Mutex::new(None),
-	    subs: Mutex::new(HashSet::new()),
+            subs: Mutex::new(HashSet::new()),
         }
+    }
+
+    pub fn create_subs_message(subscription: &str, msg_type: SubBackendMsgType) -> ZmqMessage {
+        let mut buf = BytesMut::with_capacity(subscription.len() + 1);
+        buf.put_u8(msg_type as u8);
+        buf.extend_from_slice(subscription.as_bytes());
+
+	buf.freeze().into()
     }
 }
 
@@ -70,8 +83,19 @@ impl SocketBackend for SubSocketBackend {
 #[async_trait]
 impl MultiPeerBackend for SubSocketBackend {
     async fn peer_connected(self: Arc<Self>, peer_id: &PeerIdentity, io: FramedIo) {
-	println!("Connect peer");
-        let (recv_queue, send_queue) = io.into_parts();
+        let (recv_queue, mut send_queue) = io.into_parts();
+
+	let subs_msgs: Vec<ZmqMessage> = self.subs.lock().iter().map(
+	    |x| SubSocketBackend::create_subs_message(
+		x, SubBackendMsgType::SUBSCRIBE)).collect();
+
+        for message in subs_msgs.iter() {
+            send_queue
+                .send(Message::Message(message.clone()))
+                .await
+                .unwrap();
+        }
+
         self.peers.insert(peer_id.clone(), Peer { send_queue });
         self.round_robin.push(peer_id.clone());
         match &self.fair_queue_inner {
@@ -80,21 +104,6 @@ impl MultiPeerBackend for SubSocketBackend {
                 inner.lock().insert(peer_id.clone(), recv_queue);
             }
         };
-
-	let subs = (|| { (*self.subs.lock()).clone() })();
-
-	for sub in subs.iter() {
-	    let mut buf = BytesMut::with_capacity(sub.len() + 1);
-	    buf.put_u8(1);
-	    buf.extend_from_slice(sub.as_bytes());
-
-	    let message: ZmqMessage = ZmqMessage::from(buf.freeze());
-	    let mut peer = self.peers.get_mut(&peer_id).unwrap();
-
-	    peer.send_queue
-		.send(Message::Message(message.clone()))
-		.await.unwrap();
-	}
     }
 
     fn peer_disconnected(&self, peer_id: &PeerIdentity) {
@@ -116,36 +125,27 @@ impl Drop for SubSocket {
 
 impl SubSocket {
     pub async fn subscribe(&mut self, subscription: &str) -> ZmqResult<()> {
-	println!("subscribe");
-        let mut buf = BytesMut::with_capacity(subscription.len() + 1);
-        buf.put_u8(1);
-        buf.extend_from_slice(subscription.as_bytes());
-        // let message = format!("\0x1{}", subscription);
-        let message: ZmqMessage = ZmqMessage::from(buf.freeze());
-        for mut peer in self.backend.peers.iter_mut() {
-            peer.send_queue
-                .send(Message::Message(message.clone()))
-                .await?;
-        }
 	self.backend.subs.lock().insert(subscription.to_string());
-        Ok(())
+	self.process_subs(subscription, SubBackendMsgType::SUBSCRIBE).await
     }
 
     pub async fn unsubscribe(&mut self, subscription: &str) -> ZmqResult<()> {
 	self.backend.subs.lock().remove(subscription);
-        let mut buf = BytesMut::with_capacity(subscription.len() + 1);
-        buf.put_u8(0);
-        buf.extend_from_slice(subscription.as_bytes());
-        let message = ZmqMessage::from(buf.freeze());
+	self.process_subs(subscription, SubBackendMsgType::UNSUBSCRIBE).await
+    }
+
+    async fn process_subs(&mut self, subscription: &str, msg_type: SubBackendMsgType) -> ZmqResult<()> {
+        let message: ZmqMessage = SubSocketBackend::create_subs_message(
+	    subscription, msg_type);
+
         for mut peer in self.backend.peers.iter_mut() {
             peer.send_queue
                 .send(Message::Message(message.clone()))
                 .await?;
         }
-        Ok(())
+	Ok(())
     }
 }
-
 
 #[async_trait]
 impl Socket for SubSocket {
@@ -163,7 +163,7 @@ impl Socket for SubSocket {
     }
 
     fn backend(&self) -> Arc<dyn MultiPeerBackend> {
-	self.backend.clone()
+        self.backend.clone()
     }
 
     fn binds(&mut self) -> &mut HashMap<Endpoint, AcceptStopHandle> {
