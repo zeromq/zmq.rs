9a143a9b61a50b56dcc51d6115f56a2ad5eb3b84
Timothy Hobbs <timothyhobbs@seznam.cz>
2023-04-08 19:17:08 +0000

    Implement PAIR strategy

---
+++ src/pair.rs
@@ -0,0 +1,158 @@
+/// Implements the ZMQ EXPAIR communication strategy.
+///
+/// See RFC 31 https://rfc.zeromq.org/spec/31/
+///
+/// The PAIR Socket Type
+///
+/// General behavior:
+///
+/// - MAY be connected to at most one PAIR peers, and MAY both send and receive messages.
+/// - SHALL not filter or modify outgoing or incoming messages in any way.
+/// - SHALL maintain a double queue for its peer, allowing outgoing and incoming messages to be queued independently.
+/// - SHALL create a double queue when initiating an outgoing connection to a peer, and SHALL maintain the double queue whether or not the connection is established.
+/// - SHALL create a double queue when a peer connects to it. If this peer disconnects, the PAIR socket SHALL destroy its double queue and SHALL discard any messages it contains.
+/// - SHOULD constrain incoming and outgoing queue sizes to a runtime-configurable limit.
+///
+/// For processing outgoing messages:
+///
+/// - SHALL consider its peer as available only when it has a outgoing queue that is not full.
+/// - SHALL block on sending, or return a suitable error, when it has no available peer.
+/// - SHALL not accept further messages when it has no available peer.
+/// - SHALL NOT discard messages that it cannot queue.
+///
+/// For processing incoming messages:
+///
+/// - SHALL receive incoming messages from its single peer if it has one.
+/// - SHALL deliver these to its calling application.
+///
+/// Example usage can be found in ../tests/pair.rs
+///
+use crate::async_rt::block_on_read_till_set::BlockOnReadTillSet;
+use crate::codec::*;
+use crate::endpoint::Endpoint;
+use crate::error::*;
+use crate::transport::AcceptStopHandle;
+use crate::util::{Peer, PeerIdentity};
+use crate::*;
+use crate::{SocketType, ZmqResult};
+
+use async_trait::async_trait;
+use futures_util::{SinkExt, StreamExt};
+
+use std::collections::HashMap;
+use std::sync::Arc;
+
+struct PairSocketBackend {
+    pub(crate) peer: BlockOnReadTillSet<Peer>,
+    socket_monitor: Mutex<Option<mpsc::Sender<SocketEvent>>>,
+    socket_options: SocketOptions,
+}
+
+pub struct PairSocket {
+    backend: Arc<PairSocketBackend>,
+    binds: HashMap<Endpoint, AcceptStopHandle>,
+}
+
+impl Drop for PairSocket {
+    fn drop(&mut self) {
+        self.backend.shutdown();
+    }
+}
+
+impl SocketBackend for PairSocketBackend {
+    fn socket_type(&self) -> SocketType {
+        SocketType::PAIR
+    }
+
+    fn socket_options(&self) -> &SocketOptions {
+        &self.socket_options
+    }
+
+    fn shutdown(&self) {
+        //self.peer.lock().await = None;
+    }
+
+    fn monitor(&self) -> &Mutex<Option<mpsc::Sender<SocketEvent>>> {
+        &self.socket_monitor
+    }
+}
+
+#[async_trait]
+impl SocketSend for PairSocket {
+    async fn send(&mut self, msg: ZmqMessage) -> ZmqResult<()> {
+        return match *self.backend.peer.get().await {
+            Some(ref mut peer) => peer
+                .send_queue
+                .send(Message::Message(msg))
+                .await
+                .map_err(|codec_error| ZmqError::Codec(codec_error)),
+            None => panic!("Unreachable"),
+        };
+    }
+}
+
+#[async_trait]
+impl SocketRecv for PairSocket {
+    async fn recv(&mut self) -> ZmqResult<ZmqMessage> {
+        let message = match *self.backend.peer.get().await {
+            Some(ref mut peer) => peer.recv_queue.next().await,
+            None => panic!("Unreachable"),
+        };
+        match message {
+            Some(Ok(Message::Greeting(_))) => todo!(),
+            Some(Ok(Message::Command(_))) => todo!(),
+            Some(Ok(Message::Message(m))) => Ok(m),
+            Some(Err(e)) => Err(ZmqError::Codec(e)),
+            None => Err(ZmqError::NoMessage),
+        }
+    }
+}
+
+#[async_trait]
+impl Socket for PairSocket {
+    fn with_options(options: SocketOptions) -> Self {
+        Self {
+            backend: Arc::new(PairSocketBackend {
+                peer: BlockOnReadTillSet::new(),
+                socket_monitor: Mutex::new(None),
+                socket_options: options,
+            }),
+            binds: HashMap::new(),
+        }
+    }
+
+    fn backend(&self) -> Arc<dyn MultiPeerBackend> {
+        self.backend.clone()
+    }
+
+    fn binds(&mut self) -> &mut HashMap<Endpoint, AcceptStopHandle> {
+        &mut self.binds
+    }
+
+    fn monitor(&mut self) -> mpsc::Receiver<SocketEvent> {
+        let (sender, receiver) = mpsc::channel(1024);
+        self.backend.socket_monitor.lock().replace(sender);
+        receiver
+    }
+}
+
+#[async_trait]
+impl MultiPeerBackend for PairSocketBackend {
+    async fn peer_connected(self: Arc<Self>, peer_id: &PeerIdentity, io: FramedIo) {
+        if self.peer.is_set().await {
+            todo!("Refuse connection if already connected.");
+        }
+        let (recv_queue, send_queue) = io.into_parts();
+        let peer = Peer {
+            _identity: peer_id.clone(),
+            send_queue: send_queue,
+            recv_queue: recv_queue,
+        };
+        self.peer.set(peer).await;
+    }
+
+    fn peer_disconnected(&self, _peer_id: &PeerIdentity) {
+        // *self.peer.lock().await = None;
+        todo!();
+    }
+}
---
+++ tests/pair.rs
@@ -0,0 +1,81 @@
+use zeromq::prelude::*;
+use zeromq::Endpoint;
+use zeromq::ZmqMessage;
+use zeromq::__async_rt as async_rt;
+
+use futures_channel::oneshot;
+use std::time::Duration;
+
+#[async_rt::test]
+async fn test_pair_sockets() {
+    pretty_env_logger::try_init().ok();
+
+    async fn helper(bind_addr: &'static str) {
+        // We will join on these at the end to determine if any tasks we spawned
+        // panicked
+        let mut task_handles = Vec::new();
+        let payload = chrono::Utc::now().to_rfc2822();
+
+        let cloned_payload = payload.clone();
+        let (has_bound_sender, has_bound) = oneshot::channel::<Endpoint>();
+        task_handles.push(async_rt::task::spawn(async move {
+            let mut pair_socket = zeromq::PairSocket::new();
+            let bound_to = pair_socket
+                .bind(bind_addr)
+                .await
+                .unwrap_or_else(|e| panic!("Failed to bind to {}: {}", bind_addr, e));
+            has_bound_sender
+                .send(bound_to)
+                .expect("channel was dropped");
+
+            let s: String = cloned_payload.clone();
+            let m = ZmqMessage::from(s);
+            pair_socket.send(m).await.expect("Failed to send");
+            async_rt::task::sleep(Duration::from_millis(1)).await;
+
+            let errs = pair_socket.close().await;
+            if !errs.is_empty() {
+                panic!("Could not unbind socket: {:?}", errs);
+            }
+        }));
+
+        // Block until the pair socket has finished binding
+        let bound_addr = has_bound.await.expect("channel was cancelled");
+        if let Endpoint::Tcp(_host, port) = bound_addr.clone() {
+            assert_ne!(port, 0);
+        }
+
+        let cloned_payload = payload.clone();
+        let cloned_bound_addr = bound_addr.to_string();
+        task_handles.push(async_rt::task::spawn(async move {
+            let mut pair_socket = zeromq::PairSocket::new();
+            pair_socket
+                .connect(&cloned_bound_addr)
+                .await
+                .unwrap_or_else(|_| panic!("Failed to connect to {}", bind_addr));
+
+            async_rt::task::sleep(std::time::Duration::from_millis(500)).await;
+
+            let recv_message = pair_socket.recv().await.unwrap();
+            let recv_payload = String::from_utf8(recv_message.get(0).unwrap().to_vec()).unwrap();
+            assert_eq!(cloned_payload, recv_payload);
+        }));
+
+        for t in task_handles {
+            t.await.expect("Task failed unexpectedly!");
+        }
+    }
+
+    let addrs = vec![
+        "tcp://localhost:0",
+        "tcp://127.0.0.1:0",
+        "tcp://[::1]:0",
+        "tcp://127.0.0.1:0",
+        "tcp://localhost:0",
+        "tcp://127.0.0.1:0",
+        "tcp://[::1]:0",
+        "ipc://asdf.sock",
+        "ipc://anothersocket-asdf",
+    ];
+    futures_util::future::join_all(addrs.into_iter().map(helper)).await;
+}
