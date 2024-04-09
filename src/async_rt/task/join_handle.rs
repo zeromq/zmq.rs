ea16d7c27b42e5cd2d4de234965341cc8fbcca10
Kyle Kelley <rgbkrk@gmail.com>
2024-04-08 17:40:22 -0700

    create the JoinHandle

--- src/async_rt/task/join_handle.rs
+++ src/async_rt/task/join_handle.rs
@@ -2,6 +2,25 @@
 use async_std::task as rt_task;
 #[cfg(feature = "tokio-runtime")]
 use tokio::task as rt_task;
+#[cfg(feature = "smol-runtime")]
+mod rt_task {
+    use super::JoinError;
+    use smol::Task;
+    use std::future::Future;
+    use std::pin::Pin;
+    use std::task::{ready, Context, Poll};
+
+    pub struct JoinHandle<T>(pub(crate) Task<T>);
+
+    impl<T> Future for JoinHandle<T> {
+        type Output = Result<T, JoinError>;
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            let result = Pin::new(&mut self.0).poll(cx);
+            Poll::Ready(Ok(ready!(result)))
+        }
+    }
+}
 
 use super::JoinError;
 
@@ -21,6 +40,8 @@ impl<T> Future for JoinHandle<T> {
         return result.map(Ok);
         #[cfg(feature = "tokio-runtime")]
         return result.map_err(|e| e.into());
+        #[cfg(feature = "smol-runtime")]
+        return result;
     }
 }
 impl<T> From<rt_task::JoinHandle<T>> for JoinHandle<T> {
