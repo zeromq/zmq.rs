836bad6ae18351cff598055c0616afbfa1af4e1f
jessekrubin <jessekrubin@gmail.com>
2025-11-06 11:22:54 -0800

    rand v0.9.x

--- examples/weather_server.rs
+++ examples/weather_server.rs
@@ -7,16 +7,16 @@ use zeromq::*;
 
 #[async_helpers::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     println!("Start server");
     let mut socket = zeromq::PubSocket::new();
     socket.bind("tcp://127.0.0.1:5556").await?;
 
     println!("Start sending loop");
     loop {
-        let zipcode = rng.gen_range(10000..10010);
-        let temperature = rng.gen_range(-80..135);
-        let relhumidity = rng.gen_range(10..60);
+        let zipcode = rng.random_range(10000..10010);
+        let temperature = rng.random_range(-80..135);
+        let relhumidity = rng.random_range(10..60);
         socket
             .send(format!("{} {} {}", zipcode, temperature, relhumidity).into())
             .await?;
