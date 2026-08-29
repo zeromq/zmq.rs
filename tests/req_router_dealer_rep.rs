d4dbe9da7abd210054520a66ac6ff74c1454c6d6
Reid <61492567+reidliu41@users.noreply.github.com>
2026-08-29 19:15:31 +0800

    Emit disconnect events from generic socket backend (#298)
    Signed-off-by: reidliu41 <reid201711@gmail.com>

--- tests/req_router_dealer_rep.rs
+++ tests/req_router_dealer_rep.rs
@@ -55,8 +55,16 @@ mod test {
         let router_events: Vec<_> = router_monitor.collect().await;
         let dealer_events: Vec<_> = dealer_monitor.collect().await;
         let rep_events: Vec<_> = rep_monitor.collect().await;
-        assert_eq!(2, router_events.len(), "{:?}", router_events);
-        assert_eq!(2, dealer_events.len(), "{:?}", dealer_events);
+        assert_eq!(3, router_events.len(), "{:?}", router_events);
+        assert!(matches!(
+            router_events.last(),
+            Some(zeromq::SocketEvent::Disconnected(_))
+        ));
+        assert_eq!(3, dealer_events.len(), "{:?}", dealer_events);
+        assert!(matches!(
+            dealer_events.last(),
+            Some(zeromq::SocketEvent::Disconnected(_))
+        ));
         assert_eq!(1, rep_events.len(), "{:?}", rep_events);
 
         Ok(())
